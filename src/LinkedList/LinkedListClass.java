4b4bad6307abd7e48d241f252ecfb482cd1eda2e
abhishek3aj <abhishek3aj@github.com>
2016-09-11 13:26:01 +0530

    Linklist

---
+++ src/LinkedList/LinkedListClass.java
@@ -0,0 +1,95 @@
+package LinkedList;
+
+public class LinkedListClass {
+	Node head;
+	Node last;
+	LinkedListClass(){
+		head = last = null;
+	}
+	/**
+	 * Add element at end of the linkedList
+	 * @param elem
+	 */
+	public void addElementToList(Node elem){
+		if(head != null){
+			last.next = elem;
+			last = elem;
+		}
+		else if (head == null){
+			head = last = elem;
+		}
+	}
+	/**
+	 * print all the element in the LinkedList
+	 */
+	public void printList(){
+		Node cur = head;
+		while(cur != null){
+			System.out.print(cur.data + " -> ");
+			cur = cur.next;
+		}
+	}
+	/**
+	 * return head of the LinkedList
+	 * @return
+	 */
+	public Node getHead(){
+		return head;
+	}
+	
+	/**
+	 * return last element in LinkedList
+	 * @return
+	 */
+	public Node getTail(){
+		return last;
+	}
+	/**
+	 * return size of the LinkedList
+	 * @param cur
+	 * @return
+	 */
+	public int getlistLenght(Node cur){
+		if(cur == null){
+			return 0;
+		}
+		else {
+			cur = cur.next;
+			return 1+ getlistLenght(cur);
+		}
+	}
+	/**
+	 * check weather linkList contains data
+	 * @param elem 
+	 * @param data
+	 * @return
+	 */
+	public boolean searchList(Node elem, int data){
+		if(elem == null){
+			return false;
+		}
+		else if(elem.data == data){
+			return true;
+		}
+		else {
+			return(searchList(elem.next, data));
+		}
+		
+	}
+    /*
+	/*
+	 * Reverse the given linklist using recursion
+	 */
+	public Node reverseList(Node curr, Node prev){
+        if (curr.next == null) {
+            head = curr;
+            curr.next = prev;
+            return null; 
+        }
+        Node next1 = curr.next;
+        curr.next = prev;
+        reverseList(next1, curr);
+        return head;
+	}
+}						
+
---
+++ src/LinkedList/test.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+public class test {
+	public static void main(String[] args){
+		LinkedListClass list = new LinkedListClass();
+		list.addElementToList(new Node(12));
+		list.addElementToList(new Node(25));
+		list.addElementToList(new Node(13));
+		list.addElementToList(new Node(32));
+		list.addElementToList(new Node(132));
+		list.addElementToList(new Node(32));
+		list.printList();
+		System.out.println("head -> "+ list.getHead().data);
+		System.out.println("count " + list.getlistLenght(list.getHead()));
+		System.out.println("search Result "+ list.searchList(list.getHead(),138));
+		list.reverseList(list.getHead(), null);
+		list.printList();
+	}
+}
